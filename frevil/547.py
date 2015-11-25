f1ec64ca0e12f8f24cea324223c571491fda298a
frevil <850675694@qq.com>
2015-11-25 23:55:25 +0800

    the diamond

---
+++ frevil/547.py
@@ -0,0 +1,26 @@
+k1=0
+import ctypes,sys
+STD_OUTPUT_HANDLE = -11
+std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+n=int(input())
+while(1):
+	for i in range(2*n-1):	
+		if(i+1>n):
+			k=2*n-i-1
+		else:
+			k=i+1
+		ctypes.windll.kernel32.SetConsoleTextAttribute(std_out_handle, ((k+k1)//2)%16)
+		for j in range(n-k):
+			print(" ",end='')
+		for j in range(2*k-1):
+			if(j<=k-2):
+				c=chr(65+j+k1)
+			else:
+				c=chr(65+2*k-j-2+k1)
+			print(c,end='')
+		print()
+	import os
+	os.system('cls')
+	k1=k1+1
+	k1=k1%40
+input()
